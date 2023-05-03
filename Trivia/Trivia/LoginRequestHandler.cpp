d2b7f106168b0cd88ffede6a62f308381f18fd0f
gilad bourlakoff <giladbour2006@gmail.com>
2023-05-03 19:31:55 +0300

    fixed & didnt finish func:handleRequest

--- Trivia/Trivia/LoginRequestHandler.cpp
+++ Trivia/Trivia/LoginRequestHandler.cpp
@@ -4,12 +4,22 @@ enum class REQUESTS { LOGIN = 100, SIGNUP = 110 };
 
 bool LoginRequestHandler::isRequestRelevant(const RequestInfo* requestInfo) const
 {
-    if (requestInfo->id == int(REQUESTS::LOGIN) || requestInfo->id == int(REQUESTS::LOGIN))
+    if (requestInfo->id == int(REQUESTS::LOGIN) || requestInfo->id == int(REQUESTS::SIGNUP))
         return true;
     return false;
 }
 
 RequestResult LoginRequestHandler::handleRequest(const RequestInfo* requestInfo) const 
 {
-    //?
+    RequestResult result;
+    if (isRequestRelevant(requestInfo))
+    {
+        if (requestInfo->id == int(REQUESTS::LOGIN)) {
+            //?
+        }
+        if (requestInfo->id == int(REQUESTS::SIGNUP)) {
+            //?
+        }
+    }
+    return result;
 }
