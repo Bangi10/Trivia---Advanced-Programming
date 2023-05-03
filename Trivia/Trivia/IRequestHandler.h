f921e6e09dd34cb2edd74663e7ee955e01fe9a2a
gilad bourlakoff <giladbour2006@gmail.com>
2023-05-03 18:46:07 +0300

    added functions to IRequestHandler

--- Trivia/Trivia/IRequestHandler.h
+++ Trivia/Trivia/IRequestHandler.h
@@ -1,7 +1,16 @@
 #pragma once
+#include <vector>
 
+typedef struct RequestInfo {
+	int id;
+	time_t receivalTime;
+	std::vector<unsigned char> Buffer;
+};
 
 class IRequestHandler
 {
+	virtual bool isRequestRelevant(const RequestInfo* requestInfo) const = 0;
+	virtual RequestResult handleRequest(const RequestInfo* requestInfo) const = 0;
+	virtual ~IRequestHandler();
 
 };
