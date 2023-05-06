d635cdda6a8b172a2fa79a4bb4fecc1ac3f47c69
unknown <shcraft10@gmail.com>
2023-05-06 18:55:40 +0300

    expanded python client to support login and signup

--- client_hw16.py
+++ client_hw16.py
@@ -1,7 +1,7 @@
 import socket
 import enum
 SERVER_IP = "127.0.0.1"
-SERVER_PORT = 18820
+SERVER_PORT = 8826
 WANTED_MSG = "Hello"
 
 class REQUESTS(enum.Enum):
@@ -18,19 +18,46 @@ def main():
         sock.connect(server_address)
 
         # getting a msg from server
-        server_msg = sock.recv(5)
-        server_msg = server_msg.decode()
-        print(server_msg)
+        server_msg = sock.recv(5).decode()
+        print(f"Server: {server_msg}")
 
         # sending a msg to the server
-        if(server_msg == WANTED_MSG):
-            sock.sendall(WANTED_MSG.encode())
+        request_parameters, code = get_request_parameters()
+        client_msg = create_encoded_request(request_parameters, code)
+        sock.sendall(client_msg)
+        print(f"Client: {client_msg}")
 
+        # getting a msg from server
+        server_msg = sock.recv(1024).decode()
+        print(f"Server: {server_msg}")
     except Exception as e:
             print("Error: ", e)
 
-def create_request(pars: dict):
-    buffer = []
-    buffer.append(bytes)
+def get_request_parameters():
+    login_or_signup = input("l for login, anything else for signup: ")
+    request_parameters = dict()
+    request_parameters["username"] = input("Username: ")
+    request_parameters["password"] = input("Password: ")
+
+    if login_or_signup != "l":
+        request_parameters["mail"] = input("Mail: ")
+        return request_parameters, REQUESTS.SIGNUP.value
+
+    return request_parameters, REQUESTS.LOGIN.value
+
+
+def create_encoded_request(request_parameters: dict, code: int):
+    request = b""
+    request_content = str(request_parameters)
+
+    request += code.to_bytes(1, "big")
+    request += len(request_content).to_bytes(4, "big")
+    request += request_content.encode()
+
+    return request
+
+
+
+
 if __name__ ==  "__main__":
     main()
