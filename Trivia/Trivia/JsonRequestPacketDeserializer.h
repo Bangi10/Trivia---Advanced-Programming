7f9a949a3a66b378dabfcc2143ebb04885fad5b2
Bangi10 <shcraft10@gmail.com>
2023-05-17 18:24:03 +0300

    added more deserialize requests

--- Trivia/Trivia/JsonRequestPacketDeserializer.h
+++ Trivia/Trivia/JsonRequestPacketDeserializer.h
@@ -4,15 +4,42 @@
 #include <string>
 #include <sstream>
 
-typedef struct LoginRequest {
+struct LoginRequest {
 	std::string username;
 	std::string password;
 };
 
-typedef struct SignupRequest {
+struct SignupRequest {
 	std::string username;
 	std::string password;
 	std::string email;
+};
+
+struct CreateRoomRequest {
+	std::string roomName;
+	unsigned int maxUsers;
+	unsigned int questionCount;
+	unsigned int answerTimeout;
+};
+
+struct GetPlayersInRoomRequest {
+	unsigned int roomId;
+};
+
+struct JoinRoomRequest {
+	unsigned int roomId;
+};
+
+//NO DATA REQUESTS
+struct HighScoreRequest {
+	
+};
+
+struct LogoutRequest {
+
+};
+struct GetRoomRequest {
+
 };
 
 class JsonRequestPacketDeserializer
@@ -20,4 +47,8 @@ class JsonRequestPacketDeserializer
 public:
 	static LoginRequest deserializeLoginRequest(const std::vector<unsigned char>& buffer);
 	static SignupRequest deserializeSignupRequest(const std::vector<unsigned char>& buffer);
+	static GetPlayersInRoomRequest deserializeGetPlayersRequest(const std::vector<unsigned char>& buffer);
+	static JoinRoomRequest JoinRoomRequestdeserializeJoinRoomRequest(const std::vector<unsigned char>& buffer);
+	static CreateRoomRequest deserializeCreateRoomRequest(const std::vector<unsigned char>& buffer);
+	//for HighScoreRequest, LogoutRequest, GetRoomRequest an empty struct will be created
 };
