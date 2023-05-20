6f9a2d081b71d25c8eb64435e846c043c979599e
gilad bourlakoff <giladbour2006@gmail.com>
2023-05-21 00:32:39 +0300

    added:sqlite3,IDatabase,SqliteDatabase.fixed:LoginRequestHandler

--- Trivia/Trivia/IRequestHandler.h
+++ Trivia/Trivia/IRequestHandler.h
@@ -21,5 +21,4 @@ public:
 	virtual bool isRequestRelevant(const RequestInfo& requestInfo) const = 0;
 	virtual RequestResult handleRequest(const RequestInfo& requestInfo) = 0;
 	virtual ~IRequestHandler() = default;
-
 };
