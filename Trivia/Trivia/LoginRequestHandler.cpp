165b9a7b653703edb465ef887552cfcc91f50f14
DovIchilov <dov.ichilov@gmail.com>
2023-05-20 23:17:14 +0000

    Revert "Merge branch 'feature/DB_Table_Support_Users' into 'v1.0.3'"
    This reverts merge request !13

--- Trivia/Trivia/LoginRequestHandler.cpp
+++ Trivia/Trivia/LoginRequestHandler.cpp
@@ -1,6 +1,5 @@
 #include "LoginRequestHandler.h"
 #include "JsonRequestPacketDeserializer.h"
-#include "JsonResponsePacketSerializer.h"
 
 bool LoginRequestHandler::isRequestRelevant(const RequestInfo& requestInfo) const
 {
@@ -9,21 +8,25 @@ bool LoginRequestHandler::isRequestRelevant(const RequestInfo& requestInfo) cons
     return false;
 }
 
-RequestResult LoginRequestHandler::handleRequest(const RequestInfo& requestInfo)
+RequestResult LoginRequestHandler::handleRequest(const RequestInfo& requestInfo) 
 {
     RequestResult result;
-    result.newHandler.reset(this);
+    result.newHandler = this;
     if (isRequestRelevant(requestInfo))
     {
         if (requestInfo.id == int(REQUESTS::LOGIN)) {
             LoginRequest login = JsonRequestPacketDeserializer::deserializeLoginRequest(requestInfo.buffer);
-            result.response.push_back(char(RESPONSES::LOGIN::SUCCESS));
+            result.response.push_back(char(RESPONSES::LOGIN_Y));
         }
         if (requestInfo.id == int(REQUESTS::SIGNUP)) {
             SignupRequest signup = JsonRequestPacketDeserializer::deserializeSignupRequest(requestInfo.buffer);
-            result.response.push_back(char(RESPONSES::SIGNUP::SUCCESS));
+            result.response.push_back(char(RESPONSES::SIGNUP_Y));
         }
     }
     return result;
 }
 
+LoginRequestHandler::~LoginRequestHandler()
+{
+
+}
