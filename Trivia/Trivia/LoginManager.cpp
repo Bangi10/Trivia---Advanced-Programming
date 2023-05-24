fcc9649afcb1858f6f2a0989c8c0afbc6fc797d2
gilad bourlakoff <giladbour2006@gmail.com>
2023-05-24 20:54:06 +0300

    fixed m_database to be weak_ptr,changed functions to bool

--- Trivia/Trivia/LoginManager.cpp
+++ Trivia/Trivia/LoginManager.cpp
@@ -3,63 +3,69 @@
 
 LoginManager::LoginManager()
 {
-	this->m_database->open();
+	this->m_database.lock()->open();
 }
 
 LoginManager::~LoginManager()
 {
-	delete[] this->m_database;
 	this->m_LoggedUsers.clear();
 }
 
-void LoginManager::sigup(const std::string& username, const std::string& password, const std::string& email)
+bool LoginManager::sigup(const std::string& username, const std::string& password, const std::string& email)
 {
-	if (this->m_database->doesUserExists(username))
+	if (this->m_database.lock()->doesUserExists(username))
 	{
 		std::cout << "signup error:user already exists" << std::endl;
+		return false;
 	}
 	else
 	{
-		this->m_database->addNewUser(username, password, email);
+		this->m_database.lock()->addNewUser(username, password, email);
 		LoggedUser user(username);
 		this->m_LoggedUsers.push_back(user);
+		return true;
 	}
 }
 
-void LoginManager::login(const std::string& username, const std::string& password)
+bool LoginManager::login(const std::string& username, const std::string& password)
 {
-	if (this->m_database->doesUserExists(username))
+	if (this->m_database.lock()->doesUserExists(username))
 	{
-		if (this->m_database->doesPasswordMatch(username, password))
+		if (this->m_database.lock()->doesPasswordMatch(username, password))
 		{
 			LoggedUser user(username);
 			this->m_LoggedUsers.push_back(user);
+			return true;
 		}
 		else
 		{
 			std::cout << "login error:password does not match user" << std::endl;
+			return false;
 		}
 	}
 	else
 	{
 		std::cout << "login error:user does not exists" << std::endl;
+		return false;
 	}
 }
 
-void LoginManager::logout(const std::string& username)
+bool LoginManager::logout(const std::string& username)
 {
-	if (this->m_database->doesUserExists(username))
+	if (this->m_database.lock()->doesUserExists(username))
 	{
 		for (auto it = this->m_LoggedUsers.begin(); it != this->m_LoggedUsers.end(); it++)
 		{
 			if (it->getUsername() == username)
 			{
 				this->m_LoggedUsers.erase(it);
+				return true;
 			}
 		}
 	}
 	else
 	{
 		std::cout << "logout error:user does not exists" << std::endl;
+		return false;
 	}
 }
--- Trivia/Trivia/LoginManager.h
+++ Trivia/Trivia/LoginManager.h
@@ -2,17 +2,18 @@
 #include "IDatabase.h"
 #include <vector>
 #include "LoggedUser.h"
+#include <memory>
 
 class LoginManager
 {
 public:
 	LoginManager();
 	~LoginManager();
-	void sigup(const std::string& username, const std::string& password, const std::string& email);
-	void login(const std::string& username, const std::string& password);
-	void logout(const std::string& username);
+	bool sigup(const std::string& username, const std::string& password, const std::string& email);
+	bool login(const std::string& username, const std::string& password);
+	bool logout(const std::string& username);
 
 private:
-	IDatabase* m_database;
+	std::weak_ptr<IDatabase> m_database;
 	std::vector<LoggedUser> m_LoggedUsers;
 };
