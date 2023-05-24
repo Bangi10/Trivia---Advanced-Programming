fa01d83a7f7a3b4020f354d997a829d85b86b6c4
gilad bourlakoff <giladbour2006@gmail.com>
2023-05-24 17:08:09 +0300

    added files from feature/DB_Table_Support_Users

--- Trivia/Trivia/Communicator.h
+++ Trivia/Trivia/Communicator.h
@@ -1,17 +1,11 @@
 #pragma once
-#include <iostream>
-#include <queue>
-//#include <mutex>
-//#include <condition_variable>
 #include <WinSock2.h>
-#include "LoginRequestHandler.h"
 #include <map>
-#include <string>
-#include <thread>
 #include <memory>
+#include "IRequestHandler.h"
 
 enum SERVER_DETAILS{SERVER_PORT = 8826, IFACE = 0};
-enum MESSAGE_DETAILS{MSG_LEN = 5, FLAGS = 0};
+
 class Communicator
 {
 public:
@@ -24,11 +18,5 @@ private:
 	std::map<SOCKET, std::unique_ptr<IRequestHandler>> m_clients;
 
 	void bindAndListen();
-	void handleNewClient(SOCKET sock);
-
-	//std::queue<std::string> _messageHandler;
-
-	//std::mutex _mtxReceivedMessages;
-	//std::condition_variable _msgQueueCondition;
-
+	void handleNewClient(const SOCKET sock);
 };
