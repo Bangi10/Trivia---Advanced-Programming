aabbc022b3ae8711f677bd2212fc339a2b25f625
USER <shcraft10@gmail.com>
2023-06-05 23:04:02 +0300

    started serilization

--- Trivia Client/Trivia Client/Code/JsonSerialization.cs
+++ Trivia Client/Trivia Client/Code/JsonSerialization.cs
@@ -6,10 +6,29 @@ using System.Text;
 using System.Threading.Tasks;
 using Json = System.Text.Json;
 
+
 namespace Trivia_Client.Code
 {
-    public static class JsonSerialization
+    public record struct LoginResponse(byte status);
+    public record struct SignupResponse(byte status);
+    public record struct LogoutResponse(byte status);
+    public record struct GetRoomsResponse(byte status, List<RoomData> rooms);
+    public record struct RoomData(uint id, string name, uint maxPlayers, uint numOfQuestionsInGame, uint timePerQuestion, uint roomStatus);
+    public record struct GetPlayersInRoomResponse(byte status, List<string> players);
+    public record struct getHighScoreResponse(byte status, List<string> statistics);
+    public record struct getPersonalStatusResponse(byte status, List<string> statistics);
+    public record struct JoinRoomResponse(byte status);
+    public record struct CreateRoomResponse(byte status);
+
+
+    public class JsonSerialization
     {
-        
+        public static byte[] serializeResponse<ResponseType>(ResponseType response, byte status) //even though status is in response, isn't accessible
+        {
+            List<byte> responseBuffer = new List<byte>();
+            //responseBuffer.Add(response);
+            
+        }
+
     }
 }
--- Trivia Client/Trivia Client/MainWindow.xaml.cs
+++ Trivia Client/Trivia Client/MainWindow.xaml.cs
@@ -12,19 +12,28 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Trivia_Client.Code;
 using Trivia_Client.Pages;
+using Json = System.Text.Json;
 
 namespace Trivia_Client
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+    public abstract record Response(uint status);
+    public record ErrorResponse(uint status, string message) : Response(status);
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
             mainFrame.Content = new Pages.Login();
+
+            Response response = new ErrorResponse(3, "s");
+            var jsonString = Json.JsonSerializer.Serialize(ErrorResponse(response));
+            Console.WriteLine(jsonString);
         }
     }
 }
