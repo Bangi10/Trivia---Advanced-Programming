7e8367a61840ac61b54685d7817193e8aedd4ffa
USER <shcraft10@gmail.com>
2023-06-05 19:10:58 +0300

    added json serializer empty class

---
+++ Trivia Client/Trivia Client/Code/JsonSerialization.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using Json = System.Text.Json;
+
+namespace Trivia_Client.Code
+{
+    public static class JsonSerialization
+    {
+        
+    }
+}
