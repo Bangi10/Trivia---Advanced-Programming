252f927ec528e5e431aebef75a4f44a039bea729
gilad bourlakoff <giladbour2006@gmail.com>
2023-06-21 00:14:10 +0300

    added files from feature/v3.0.0/fix_communication

---
+++ Trivia Client/Trivia Client/Pages/CreateRoom.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Trivia_Client.Code;
+
+namespace Trivia_Client.Pages
+{
+    /// <summary>
+    /// Interaction logic for CreateRoom.xaml
+    /// </summary>
+    public partial class CreateRoom : Page
+    {
+        public CreateRoom()
+        {
+            InitializeComponent();
+            usernameLabel.Content = User.Instance().GetUsername();
+        }
+        private void CreateRoom_Click(object sender, RoutedEventArgs e)
+        {
+            //needs to add playerm if he doesnt exists
+            CreateRoomRequest request = new CreateRoomRequest(roomName.Text, Convert.ToUInt32(maxPlayers.Text),
+                                                              Convert.ToUInt32(numberOfQuestions.Text), Convert.ToUInt32(timePerQuestion.Text));
+            byte[] requestBuffer = JsonSerialization.serializeRequest<CreateRoomRequest>(request, RequestsCodes.CREATE_ROOM);
+            ClientCommuinactor comm = ClientCommuinactor.Instance;
+
+            comm.sendBytes(requestBuffer);
+
+            var readTuple = comm.readBytes();
+            byte[] jsonBuffer = readTuple.Item1;
+            byte code = readTuple.Item2;
+
+            if (Helper.isInEnum<ResponseCodes.ERRORS>(code))
+            {
+                ErrorResponse response = JsonSerialization.deserializeResponse<ErrorResponse>(jsonBuffer);
+                ErrorLabel.Content = response.message;
+            }
+            else if (code == (byte)ResponseCodes.ROOM.CREATED_ROOM)
+            {
+                User.Instance().SetIsRoomAdmin(true);
+                User.Instance().SetRoom(new RoomData(0, roomName.Text, Convert.ToUInt32(maxPlayers.Text), Convert.ToUInt32(numberOfQuestions.Text), Convert.ToUInt32(timePerQuestion.Text), 0));
+                NavigationService?.Navigate(new Room());
+            }
+        }
+        //private RoomData GetRoomFromServer(string _roomName)
+        //{
+        //    var rooms = GetRooms();
+        //    RoomData requestedRoom = new RoomData();
+        //    foreach (var room in rooms)
+        //    {
+        //        if (room.name == _roomName)
+        //        {
+        //            requestedRoom = room;
+        //        }
+        //    }
+        //    return requestedRoom;
+        //}
+        private List<RoomData> GetRooms()
+        {
+            byte[] requestBuffer = JsonSerialization.serializeRequestCode(RequestsCodes.GET_ROOMS);
+            ClientCommuinactor comm = ClientCommuinactor.Instance;
+            Tuple<byte[], byte> readTuple;
+
+            comm.sendBytes(requestBuffer);
+            readTuple = comm.readBytes();
+
+            byte[] jsonBuffer = readTuple.Item1;
+            byte code = readTuple.Item2;
+
+            GetRoomsResponse response = JsonSerialization.deserializeResponse<GetRoomsResponse>(jsonBuffer);
+            return response.rooms;
+        }
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            //will trigger DataWindow_Closing
+            Application.Current.Shutdown();
+
+        }
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService?.Navigate(new Pages.MainMenu());
+        }
+        
+
+    }
+}
---
+++ Trivia Client/Trivia Client/Pages/MyStats.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Trivia_Client.Code;
+
+namespace Trivia_Client.Pages
+{
+    /// <summary>
+    /// Interaction logic for MyStats.xaml
+    /// </summary>
+    public partial class MyStats : Page
+    {
+        public MyStats()
+        {
+            InitializeComponent();
+            User user = User.instance;
+            numOfGamesPlayed.Text = user.GetGamesPlayed().ToString();
+            numOfRightAnswers.Text = user.GetRightAnswers().ToString();
+            numOfWrongAnswers.Text = (user.GetTotalAnswers()-user.GetRightAnswers()).ToString();
+            avgTimeForAnswer.Text = user.GetAvgTimeForAnswer().ToString();
+            usernameLabel.Content = user.GetUsername();
+        }
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService?.Navigate(new Pages.Statistics());
+        }
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            //will trigger DataWindow_Closing
+            Application.Current.Shutdown();
+        }
+    }
+}
